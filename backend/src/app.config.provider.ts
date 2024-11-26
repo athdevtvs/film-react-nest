68bdff6943255153f2754ac91845f8b00d08db83
Tatiana Vaganova <athdev@yandex.ru>
2024-11-26 18:02:33 +0300

    feat: read environment variables within app config provider file

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -1,18 +1,17 @@
-import {ConfigModule} from "@nestjs/config";
-
-export const configProvider = {
-    imports: [ConfigModule.forRoot()],
-    provide: 'CONFIG',
-    useValue: < AppConfig> {
-        //TODO прочесть переменнные среды
-    },
-}
+import { registerAs } from '@nestjs/config';
 
 export interface AppConfig {
-    database: AppConfigDatabase
+  database: AppConfigDatabase;
 }
 
 export interface AppConfigDatabase {
-    driver: string
-    url: string
+  driver: string;
+  url: string;
 }
+
+export const configProvider = registerAs<AppConfig>('app', () => ({
+  database: {
+    url: process.env.DB_URL ?? 'mongodb://localhost:27017/afisha',
+    driver: process.env.DB_DRIVER ?? 'mongodb',
+  },
+}));

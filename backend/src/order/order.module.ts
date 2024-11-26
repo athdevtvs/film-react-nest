37cb27acb2d9fcadb1c12ca2b7116dd9edc4b034
Tatiana Vaganova <athdev@yandex.ru>
2024-11-26 19:53:09 +0300

    feat: configure films module for managing order-related functionality

--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { OrderController } from './order.controller';
+import { ConfigModule } from '@nestjs/config';
+import { OrdersRepository } from '../repository/order.repository';
+import { OrderService } from './order.service';
+import { FilmsModule } from '../films/films.module';
 
-@Module({})
+@Module({
+  imports: [ConfigModule, FilmsModule],
+  controllers: [OrderController],
+  providers: [OrdersRepository, OrderService],
+  exports: [OrderService],
+})
 export class OrderModule {}

3d9e164f4ea298fa4226e79ed6894feca36d0c13
Tatiana Vaganova <athdev@yandex.ru>
2024-11-26 18:49:15 +0300

    feat: configure films module for managing film-related functionality

--- backend/src/films/films.module.ts
+++ backend/src/films/films.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { FilmsService } from './films.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { configProvider } from '../app.config.provider';
+import { Film, FilmSchema } from './schemas/films.schema';
 import { FilmsController } from './films.controller';
+import { FilmsRepository } from '../repository/films.repository';
+import { FilmsService } from './films.service';
 
 @Module({
-  providers: [FilmsService],
+  imports: [
+    ConfigModule.forFeature(configProvider),
+    MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
+  ],
   controllers: [FilmsController],
+  providers: [FilmsRepository, FilmsService],
+  exports: [FilmsRepository],
 })
 export class FilmsModule {}

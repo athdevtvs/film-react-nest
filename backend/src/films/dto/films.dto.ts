4f154a069db7f365b5375071f00edfe1e9ce694e
Tatiana Vaganova <athdev@yandex.ru>
2024-11-26 18:16:32 +0300

    feat: create film-related DTOs for data transfer

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,43 @@
-//TODO описать DTO для запросов к /films
+export class BaseScheduleDto {
+  daytime: string;
+  hall: number;
+  rows: number;
+  seats: number;
+  price: number;
+  taken: string[];
+}
+
+export class GetScheduleDto extends BaseScheduleDto {
+  id: string;
+}
+
+export class GetFilmDto {
+  id: string;
+  rating: number;
+  director: string;
+  tags: string[];
+  title: string;
+  about: string;
+  description: string;
+  image: string;
+  cover: string;
+  schedule: GetScheduleDto[];
+}
+
+export class CreateScheduleDto extends BaseScheduleDto {}
+
+export class CreateFilmDto {
+  readonly rating: number;
+  readonly director: string;
+  readonly tags: string[];
+  readonly image: string;
+  readonly cover: string;
+  readonly title: string;
+  readonly about: string;
+  readonly description: string;
+  readonly schedule: GetScheduleDto[];
+}
+
+export class FilmScheduleParams {
+  id: string;
+}

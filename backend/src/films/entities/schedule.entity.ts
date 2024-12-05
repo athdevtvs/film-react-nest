4391dfe4f664a7c5db027a756f38df2994723e04
Tatiana Vaganova <athdev@yandex.ru>
2024-12-05 04:05:20 +0300

    feat: define models for Film and Session entities

---
+++ backend/src/films/entities/schedule.entity.ts
@@ -0,0 +1,54 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Films } from './film.entity';
+import {
+  IsNumber,
+  IsString,
+  IsPositive,
+  Min,
+  Max,
+  IsOptional,
+} from 'class-validator';
+
+@Entity()
+export class Schedules {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  @IsString()
+  daytime: string;
+
+  @Column({ type: 'int' })
+  @IsString()
+  filmId: string;
+
+  @Column({ type: 'int' })
+  @IsNumber()
+  @IsPositive()
+  hall: number;
+
+  @Column({ type: 'int' })
+  @IsString()
+  @Min(1)
+  @Max(100)
+  rows: number;
+
+  @Column({ type: 'int' })
+  @IsNumber()
+  @Min(1)
+  @Max(500)
+  seats: number;
+
+  @Column({ type: 'float' })
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @Column({ type: 'text', nullable: true })
+  @IsOptional()
+  @IsString()
+  taken: string[];
+
+  @ManyToOne(() => Films, (film) => film.schedule)
+  film: Films;
+}
